0fda5d7b5574864478ddb69d954081e8f11f4984
alarky <alarky@gmail.com>
2014-08-29 20:05:47 +0000

    fix script

--- lib/Isucon3/batch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "batch start" >> /tmp/batch.log
-. /home/isucon/env.sh
-echo "env done" >> /tmp/batch.log
-/home/isucon/local/perl-5.18/bin/carton exec perl /home/isucon/webapp/perl/lib/Isucon3/batch.pl >> /tmp/batch.log 2>&1
